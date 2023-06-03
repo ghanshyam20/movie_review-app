e2bc964e86fc8ad4be862f1045c41d2718252d47
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2023-06-03 16:37:31 +0545

    certain changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from .forms import UserCreateForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
@@ -8,65 +7,42 @@ from django.shortcuts import redirect
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
 
-
 def signupaccount(request):
-    if request.method == "GET":
-        return render(request, "signupaccount.html", {"form": UserCreateForm})
-
+    if request.method == 'GET':
+        return render(request, 'signupaccount.html', 
+                       {'form':UserCreateForm})
     else:
-        if request.POST["password1"] == request.POST["password2"]:
+        if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"]
-                )
+                user = User.objects.create_user(request.POST['username'], 
+                            password=request.POST['password1'])
                 user.save()
                 login(request, user)
-                return redirect("home")
+                return redirect('home')
             except IntegrityError:
-                return render(
-                    request,
-                    "signupaccount.html",
-                    {
-                        "form": UserCreateForm,
-                        "error": "Username already taken Choose new username.",
-                    },
-                )
-
+                return render(request, 'signupaccount.html', 
+                 {'form':UserCreateForm,
+                 'error':'Username already taken. Choose new username.'})
         else:
-            return render(
-                request,
-                "signupaccount.html",
-                {"form": UserCreateForm, "error": "Passwords do not match"},
-            )
+            return render(request, 'signupaccount.html', 
+             {'form':UserCreateForm, 'error':'Passwords do not match'})
 
 @login_required
-def logoutaccount(request):
+def logoutaccount(request):        
     logout(request)
-    return redirect("home")
+    return redirect('home')
 
-
-def loginaccount(request):
-    if request.method == "GET":
-        return render(request, "loginaccount.html", {"form": AuthenticationForm})
+def loginaccount(request):    
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', 
+                      {'form':AuthenticationForm})
     else:
-        user = authenticate(
-            request,
-            username=request.POST["username"],
-            password=request.POST["password"],
-        )
+        user = authenticate(request, username=request.POST['username'],
+                            password=request.POST['password'])
         if user is None:
-            return render(
-                request,
-                "loginaccount.html",
-                {
-                    "form": AuthenticationForm(),
-                    "error": "username and password do not match",
-                }
-            )
-
-        else:
-            login(request, user)
-            return redirect("home")
-
-
-
+            return render(request,'loginaccount.html', 
+                    {'form': AuthenticationForm(), 
+                    'error': 'username and password do not match'})
+        else: 
+            login(request,user)
+            return redirect('home')
--- movie/forms.py
+++ movie/forms.py
@@ -1,41 +1,20 @@
-from django.forms import ModelForm ,Textarea
+from django.forms import ModelForm, Textarea
 from .models import Review
 
-
 class ReviewForm(ModelForm):
-    def __init__(self,*args,**kwargs):
-        super(ModelForm,self).__init__(*args,**kwargs)
-
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
         self.fields['text'].widget.attrs.update(
-            {'class':'form-control'})
+                            {'class': 'form-control'})
         self.fields['watchAgain'].widget.attrs.update(
-            {'class':'form-check-input'}
-        )
-        class Meta:
-            model=Review
-            fields=['text','watchAgain']
-            labels={
-                'watchAgain':('Watch Again')
-            }
-            widgets={
-                'text':Textarea(attrs={'rows':4}),
-            }
-# class ReviewForm(ModelForm):
-#      def __init__(self, *args, **kwargs):
-#             super(ModelForm, self).__init__(*args,
-#             **kwargs)
-#             self.fields['text'].widget.attrs.update(
-#             {'class': 'form-control'})
-#             self.fields['watchAgain'].widget.attrs.update(
-#             {'class': 'form-check-input'})
+                                  {'class': 'form-check-input'})
 
-#             class Meta:
-#                  model=Review
-#                  fields=['text','watchAgain']
-
-
-
-     
-
-
-
+    class Meta:
+        model = Review
+        fields = ['text','watchAgain']    
+        labels = {
+            'watchAgain': ('Watch Again')
+        }
+        widgets = {
+            'text': Textarea(attrs={'rows': 4}),
+        }
